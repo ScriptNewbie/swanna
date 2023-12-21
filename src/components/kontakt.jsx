66bd772dceaa8b1f12e836bd88a90ba236331517
Marcin Konopka <marcin.konopka98@protonmail.com>
2023-12-21 19:01:59 +0100

    Map component extracted

--- src/components/kontakt.jsx
+++ src/components/kontakt.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import $ from "jquery";
+import Map from "./map";
 
 function Kontakt({ setStyle }) {
   const loaded = useRef(false);
@@ -120,61 +121,11 @@ function Kontakt({ setStyle }) {
               </div>
             </center>
           </div>
-          <div
-            id="map"
-            style={{
-              position: "relative",
-              height: "600px",
-              marginTop: "15px",
-              marginBottom: "-15px",
-              opacity: 0,
-            }}
-          >
-            <div
-              id="mapwrap"
-              style={{
-                position: "absolute",
-                border: 0,
-                left: "-15px",
-              }}
-            >
-              <iframe
-                title={"tg" + Date.now().toString() + Math.random().toString()}
-                id="tg"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d81269.44827176395!2d18.77198715188062!3d50.46586470587073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471129ee71a16e17%3A0xd069d15a6f1c3f94!2sTarnowskie+G%C3%B3ry!5e0!3m2!1spl!2spl!4v1500586120424"
-                frameBorder="0"
-                style={{
-                  position: "absolute",
-                  width: "740px",
-                  height: "600px",
-                }}
-              ></iframe>
-              <iframe
-                title={
-                  "kscl" + Date.now().toString() + Math.random().toString()
-                }
-                id="kscl"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9833.1220241138!2d18.83755033754838!3d50.43553763422281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x57731b1c74e7bd22!2zS2_Fm2Npw7PFgiBwLncuIMWbdy4gQW5ueQ!5e0!3m2!1spl!2spl!4v1500573216123"
-                frameBorder="0"
-                style={{
-                  position: "absolute",
-                  width: "740px",
-                  height: "600px",
-                }}
-              ></iframe>
-              <iframe
-                title={
-                  "kapl" + Date.now().toString() + Math.random().toString()
-                }
-                id="kapl"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9679.660958362772!2d18.83830598088312!3d50.42819036350281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x65d19ba30a1ef36e!2sParafia+Rzymsko-katolicka+p.w.+%C5%9Bw.+Anny+-+Probostwo!5e0!3m2!1spl!2spl!4v1500590667015"
-                frameBorder="0"
-                style={{
-                  position: "absolute",
-                  width: "740px",
-                  height: "600px",
-                }}
-              ></iframe>
+          <div id="map">
+            <div id="mapwrap">
+              <Map id="tg" />
+              <Map id="kscl" />
+              <Map id="kapl" />
             </div>
           </div>
         </div>
---
+++ src/components/map.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const maps = {
+  tg: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d81269.44827176395!2d18.77198715188062!3d50.46586470587073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471129ee71a16e17%3A0xd069d15a6f1c3f94!2sTarnowskie+G%C3%B3ry!5e0!3m2!1spl!2spl!4v1500586120424",
+  kscl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9833.1220241138!2d18.83755033754838!3d50.43553763422281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x57731b1c74e7bd22!2zS2_Fm2Npw7PFgiBwLncuIMWbdy4gQW5ueQ!5e0!3m2!1spl!2spl!4v1500573216123",
+  kapl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9679.660958362772!2d18.83830598088312!3d50.42819036350281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x65d19ba30a1ef36e!2sParafia+Rzymsko-katolicka+p.w.+%C5%9Bw.+Anny+-+Probostwo!5e0!3m2!1spl!2spl!4v1500590667015",
+};
+
+function Map({ id, map }) {
+  return (
+    <iframe
+      className="googleMap"
+      title={id + Date.now().toString() + Math.random().toString()}
+      id={id}
+      src={maps[id]}
+      frameBorder="0"
+    ></iframe>
+  );
+}
+
+export default Map;
