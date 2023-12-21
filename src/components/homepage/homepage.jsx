92498ba324d1d665f0447c6f4b7bff5bc8f45e5d
Marcin Konopka <marcin.konopka98@protonmail.com>
2023-12-21 11:27:58 +0100

    bugfixes and removed id named "fix" from css

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -6,28 +6,32 @@ import News from "./news";
 import Delimiter from "./delimiter";
 
 let blockHome = false;
-function Homepage() {
+function Homepage({ setStyle, history }) {
   const [content, setContent] = useState([]);
 
-  useEffect(async () => {
+  useEffect(() => {
     $("#maincontent").css({ height: "var(--end_height)" });
     blockHome = false;
-    try {
-      const { data: content } = await axios.get(
-        "https://api.swanna.net.pl/index.php"
-      );
-      setContent(content);
-    } catch (e) {
-      const content = [
-        {
-          nazwa: "Błąd połączenia z bazą danych",
-          data: "01/01/1970",
-          tresc:
-            "Z powodu błędu połączenia z bazą danych wyświetlenie aktualności jest niemożliwe. Pozostałe podstrony powinny działać poprawnie.",
-        },
-      ];
-      setContent(content);
-    }
+    const fetchData = async () => {
+      try {
+        const { data: content } = await axios.get(
+          "https://api.swanna.net.pl/index.php"
+        );
+        setContent(content);
+      } catch (e) {
+        const content = [
+          {
+            nazwa: "Błąd połączenia z bazą danych",
+            data: "01/01/1970",
+            tresc:
+              "Z powodu błędu połączenia z bazą danych wyświetlenie aktualności jest niemożliwe. Pozostałe podstrony powinny działać poprawnie.",
+          },
+        ];
+        setContent(content);
+      }
+    };
+
+    fetchData();
     return () => {
       $("#maincontent").css({ height: "" });
     };
@@ -37,7 +41,7 @@ function Homepage() {
     if (!blockHome) {
       let height = $("#newsContent")[0].offsetHeight + 30;
       if (height > 30) {
-        this.props.setStyle(740, height);
+        setStyle(740, height);
         $("#maincontent").css({ height: "" });
         blockHome = true;
       }
@@ -46,34 +50,32 @@ function Homepage() {
 
   return (
     <div id="newsContent" className="hghKeep">
-      <div id="fix">
-        {content.map((post, index) => (
-          <div key={post.id}>
-            <News
-              title="Dane parafii"
-              date={"Przypięty"}
-              content={
-                <p>
-                  Dane parafii tj. adres, telefon czy numer konta bankowego
-                  znajdziesz na podstronie{" "}
-                  <a
-                    id="redToCon"
-                    href="/kontakt"
-                    onClick={(e) => {
-                      tools.changeScreen(e, "/kontakt", this.props.history);
-                    }}
-                  >
-                    kontakt!
-                  </a>
-                </p>
-              }
-            />
-            <Delimiter />
-            <News title={post.nazwa} date={post.data} content={post.tresc} />
-            <Delimiter index={index} lastIndex={content.length - 1} />
-          </div>
-        ))}
-      </div>
+      {content.map((post, index) => (
+        <div key={post.id}>
+          <News
+            title="Dane parafii"
+            date={"Przypięty"}
+            content={
+              <p>
+                Dane parafii tj. adres, telefon czy numer konta bankowego
+                znajdziesz na podstronie{" "}
+                <a
+                  id="redToCon"
+                  href="/kontakt"
+                  onClick={(e) => {
+                    tools.changeScreen(e, "/kontakt", history);
+                  }}
+                >
+                  kontakt!
+                </a>
+              </p>
+            }
+          />
+          <Delimiter />
+          <News title={post.nazwa} date={post.data} content={post.tresc} />
+          <Delimiter index={index} lastIndex={content.length - 1} />
+        </div>
+      ))}
     </div>
   );
 }
