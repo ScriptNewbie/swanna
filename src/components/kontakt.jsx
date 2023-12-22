33eeb9a8aeecfb26888d560feb80dd14d2f216e5
Marcin Konopka <marcin.konopka98@protonmail.com>
2023-12-22 01:00:10 +0100

    Kontakt refactor

--- src/components/kontakt.jsx
+++ src/components/kontakt.jsx
@@ -1,58 +1,13 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import $ from "jquery";
 import Map from "./map";
 
 function Kontakt({ setStyle }) {
-  const loaded = useRef(false);
-  const time = useRef(null);
-
-  const showKapl = () => {
-    if (loaded.current) {
-      $("#map").css({ animation: "fadeout 400ms 1" });
-      setTimeout(() => {
-        $("#map").css({ animation: "fadein 300ms 1" });
-        $("#kscl").css({ "z-index": 4 });
-        $("#kapl").css({ "z-index": 5 });
-      }, 200);
-    } else {
-      $("#kscl").css({ "z-index": 4 });
-      $("#kapl").css({ "z-index": 5 });
-    }
-  };
-
-  const showKscl = () => {
-    if (loaded.current) {
-      $("#map").css({ animation: "fadeout 400ms 1" });
-      setTimeout(() => {
-        $("#map").css({ animation: "fadein 300ms 1" });
-        $("#kscl").css({ "z-index": 5 });
-        $("#kapl").css({ "z-index": 4 });
-      }, 200);
-    } else {
-      $("#kscl").css({ "z-index": 5 });
-      $("#kapl").css({ "z-index": 4 });
-    }
-  };
-
+  const [currentMap, setCurrentMap] = useState("tg");
   useEffect(() => {
-    $("#tg").css({ "z-index": 3 });
-    $("#kscl").css({ "z-index": 2 });
-    $("#kapl").css({ "z-index": 1 });
     let height = $("#contactContent")[0].offsetHeight + 30;
     setStyle(740, height);
-    $("#tg").on("load", () => {
-      $("#map").css({ animation: "fadein 3s 1 2s" });
-      time.current = setTimeout(() => {
-        loaded.current = true;
-        $("#map").css({ opacity: 1 });
-      }, 4900);
-    });
-
-    return () => {
-      clearTimeout(time.current);
-    };
   }, []);
-
   return (
     <div id="contactContent">
       <div className="grid-container">
@@ -92,7 +47,9 @@ function Kontakt({ setStyle }) {
               type="button"
               className="btn btn-secondary"
               style={{ marginTop: "10px" }}
-              onClick={showKscl}
+              onClick={() => {
+                setCurrentMap("kscl");
+              }}
             >
               Pokaż na mapie
             </button>
@@ -104,7 +61,9 @@ function Kontakt({ setStyle }) {
               type="button"
               className="btn btn-secondary"
               style={{ marginTop: "10px" }}
-              onClick={showKapl}
+              onClick={() => {
+                setCurrentMap("kapl");
+              }}
             >
               Pokaż na mapie
             </button>
@@ -123,9 +82,7 @@ function Kontakt({ setStyle }) {
           </div>
           <div id="map">
             <div id="mapwrap">
-              <Map id="tg" />
-              <Map id="kscl" />
-              <Map id="kapl" />
+              <Map id={currentMap} />
             </div>
           </div>
         </div>
