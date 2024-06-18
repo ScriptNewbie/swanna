ed3b725b870a275dbf661744d38b82f3f291dacc
Marcin Konopka <marcin.konopka98@protonmail.com>
2024-06-18 18:43:31 +0200

    PDF reader fix

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,22 @@ import App from "./App.jsx";
 import { TransitionProvider } from "./contexts/TransitionContext";
 import { CookiesProvider } from "./contexts/cookiesContext";
 
+// @ts-expect-error This does not exist outside of polyfill which this is doing
+if (typeof Promise.withResolvers === "undefined") {
+  if (window)
+    // @ts-expect-error This does not exist outside of polyfill which this is doing
+    window.Promise.withResolvers = function () {
+      let resolve, reject;
+      const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+      return { promise, resolve, reject };
+    };
+}
+
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
+  "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
   import.meta.url
 ).toString();
 
